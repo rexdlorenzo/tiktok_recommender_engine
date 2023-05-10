d214bf2b40ca22d096e9599886b0e89debcbc3cb
Rex David Lorenzo <rexdavidlorenzo@gmail.com>
2023-05-10 19:00:56 +0800

    updated text for data extraction, EDA, and ML from script and added slides as images

--- pages/data_extraction_eda_ml.py
+++ pages/data_extraction_eda_ml.py
@@ -13,25 +13,60 @@ import numpy as np
 
 sns.set()
 
-def future():
+def data_extraction():
     
     # Write the title
-    st.title(
-        "Majority of Filipinos have not saved for Old Age"
-    )
-    st.markdown(
-        "**58.4%** of Filipinos have not set aside money for Old Age in the past year."
-    )
-
-    ##################################################
+    st.header("Data Extraction")
+    st.image("images/data_extraction.png")
 
     st.markdown(
         """
-        While this figure may seem discouraging, it is worth noting that a significant portion of the population was able to save for old age, \
-        especially when compared to several other countries in the East Asia & Pacific (excluding high income) region, such as Cambodia, Mongolia, Lao PDR, Indonesia \
-        and Myanmar, where less than a third of the population saved for old age, the percentage of savers in the Philippines (41.6%) is relatively high. \
-        In terms of those who saved in the past year, **the Philippines still ranked third in the region**, behind Malaysia (54.8%) and Thailand (55.6%), which had higher percentages.
+        For our data extraction process, we used the Spotipy module for Python to access the Spotify Web API. We extracted the top 5 most popular TikTok playlists, each with over 430,000 likes. After combining these playlists, we removed any duplicate tracks.
+
+        Next, we extracted the top 5 most popular playlists for each of our chosen genres, all with at least 100,000 likes. We also combined these playlists and removed any duplicates.
+
+        Finally, we removed any tracks from the genre playlists that were also found in the TikTok playlists and stored them in 2 separate CSV files.
         """
     )
 
-future()
+def eda():
+    st.header("Exploratory Data Analysis (EDA)")
+    st.image("images/eda1.png")
+    st.image("images/eda2.png")
+    st.markdown(
+        """
+        Our Exploratory Data Analysis of popular tracks on TikTok found results that may align with common beliefs about what makes them successful on the app.
+
+        First, most of the popular tiktok tracks are also popular on Spotify. 
+
+        Next, these tracks also tend to be more danceable than not. 
+
+        Additionally, popular tracks on TikTok usually have higher energy scores. Energy is a measure of intensity and activity. Energetic songs typically feel fast-paced, loud, and full of noise.
+        """
+    )
+    st.markdown(
+        """
+        Our EDA also shows that majority of the popular tiktok tracks are not acoustic, which makes sense considering which genres are common among these tracks, like EDM, hip-hop, pop, and r&b.  The exception to this trend is the Indie genre, since Indie music is most likely to have acoustic elements.
+
+        Finally, our analysis showed that most popular TikTok tracks contain vocal content as indicated by their very low instrumentalness scores. Only a few tracks have high instrumentalness, suggesting that they are likely to be instrumental.
+        """
+    )
+
+def ml():
+    st.header("Machine Learning")
+    st.image("images/ml.png")
+    st.markdown(
+    """
+    Now for our machine learning modeling:
+
+    We selected 9 audio features, including danceability, energy, acousticness, and instrumentalness. All of them are numerical. We tried adding other features like the key, mode, and time signature but they did not improve the performance. Removing any of the 9 features also reduced the performance of the model so we kept these 9 as our features.
+
+    Our target variable  is the genre, encoded as numbers from 0 to 4.   Our model is used to predict the genre of tiktok tracks, where tracks with the highest percentage confidence for each class are used as the seed tracks for recommending which songs in an artist's album have the potential of becoming popular on Tiktok PH.
+
+    We scaled the features using MinMaxScaler then run the data through tree-like classifiers such a random forest  and xgboost. We chose these type of classifiers for their strong ability to provide explainable results.
+    """
+    )
+
+data_extraction()
+eda()
+ml()
